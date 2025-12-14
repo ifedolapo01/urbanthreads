1b13bd5b6102ec2077f67e64c4e90b35196624a6
Ife <ifedolapoajayi0@gmail.com>
2025-12-14 23:02:31 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import Header from '@/components/Header';
 import { CartProvider } from '@/components/CartProvider';
 
+import { Analytics } from "@vercel/analytics/next"
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
